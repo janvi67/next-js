1cedc9d0635a5124536d2fafba8ff41f447b5119
janvi67 <janviraychura7890@gmail.com>
2024-11-08 19:04:55 +0530

    remove profile route

--- src/app/profile/page.js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const Login = dynamic(() => import("@/views/auth/login"), {
-  loading: () => <h1>loading..</h1>,
-});
-
-export default function Page() {
-  return <Login />;
-}
